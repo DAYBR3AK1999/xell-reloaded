2a8ec696bb58dedc27e89825b9ae8df1873d31b8
Mathieu <44482339+DAYBR3AK1999@users.noreply.github.com>
2025-03-07 14:14:48 +0100

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -5,6 +5,5 @@ static char* asciiart = "\n"
 "|  __  |/ _ \\ \\/ / _` | |\\/| |/ _ \\ / _` / __| / __/ _ \\| '_ ` _ \\ \n"
 "| |  | |  __/>  < (_| | |  | | (_) | (_| \\__ \\| (_| (_) | | | | | |\n"
 "|_|  |_|\\___/_/\\_\\__,_|_|  |_|\\___/ \\__,_|___(_)___\\___/|_| |_| |_|\n\n";
-
 static char* asciitail =
 	"              HexaMods.com XeLL RELOADED - Xenon Linux Loader\n\n";
