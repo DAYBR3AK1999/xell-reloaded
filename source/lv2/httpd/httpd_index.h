d2ad89f9e27e8a55d7d15cebcc6e24130e1453ce
Mathieu <44482339+DAYBR3AK1999@users.noreply.github.com>
2025-03-07 13:06:52 +0100

    Update httpd_index.h

--- source/lv2/httpd/httpd_index.h
+++ source/lv2/httpd/httpd_index.h
@@ -8,12 +8,16 @@
 #ifndef HTTPD_INDEX_H_
 #define HTTPD_INDEX_H_
 
-
-#endif /* HTTPD_INDEX_H_ */
-
 #include "httpd/httpd.h"
 
+// Function prototypes
 int response_index_process_request(struct http_state *http, const char *method, const char *url);
 int response_index_do_header(struct http_state *http);
 int response_index_do_data(struct http_state *http);
 void response_index_finish(struct http_state *http);
+
+// Console information functions
+const char *get_console_type();
+void get_temperatures(char *buffer);
+
+#endif /* HTTPD_INDEX_H_ */
